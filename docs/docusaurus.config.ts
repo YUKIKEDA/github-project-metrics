32e3765caa6f1cd8844cf78602be2494e2f63c37
YUKIKEDA <yuuuu1008ki@gmail.com>
2025-11-08 00:54:57 +0900

    chore: Update site URL in Docusaurus configuration for correct deployment

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   //--------------------------------------------------
 
   // Set the production url of your site here
-  url: 'https://YUKIKEDA/github-project-sample',
+  url: 'https://yukikeda.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/github-project-sample/',
